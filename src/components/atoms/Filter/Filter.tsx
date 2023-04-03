2aa05176a08c35bf2c37abe30a5527a9b70faadf
José Mota <jose.a.mota11@gmail.com>
2023-04-03 16:12:46 -0400

    :lipstick: Adding to the Filter component an initial design

--- src/components/atoms/Filter/Filter.tsx
+++ src/components/atoms/Filter/Filter.tsx
@@ -5,30 +5,47 @@ export type FilterProps = {
 const Filter = ({ section }: FilterProps) => (
   <div className="center--section">
     <div className="filter">
+      <h2 className="filter--title">Filter</h2>
       {section === 'CHARACTERS' ? (
-        <>
-          <input className="filter--input" type="search" />
+        <div className="filter--container__characters">
+          <input
+            className="filter--input"
+            type="search"
+            placeholder="E.g. Spider-Man"
+          />
           <select className="filter--select">
-            <option value="any">Any</option>
+            <option value="default" defaultChecked>
+              Comics (Default)
+            </option>
             <option value="test">Test</option>
           </select>
           <select className="filter--select">
-            <option value="any">Any</option>
+            <option value="default">Stories (Default)</option>
             <option value="test">Test</option>
           </select>
-        </>
+        </div>
       ) : null}
       {section === 'COMICS' ? (
-        <>
-          <input className="filter--input" type="search" />
-          <input className="filter--input" type="search" />
-        </>
+        <div className="filter--container__comics">
+          <input
+            className="filter--input"
+            type="search"
+            placeholder="E.g. comic"
+          />
+          <input
+            className="filter--input"
+            type="search"
+            placeholder="E.g. Marvel Previews (2017)"
+          />
+        </div>
       ) : null}
       {section === 'STORIES' ? (
-        <select className="filter--select">
-          <option value="any">Any</option>
-          <option value="test">Test</option>
-        </select>
+        <div className="filter--container__stories">
+          <select className="filter--select">
+            <option value="any">Any</option>
+            <option value="test">Test</option>
+          </select>
+        </div>
       ) : null}
     </div>
   </div>
