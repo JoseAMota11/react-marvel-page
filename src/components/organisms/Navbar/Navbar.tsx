eeaaf88e3cdaded9dbe87526bdb9d145dafa9022
José Mota <jose.a.mota11@gmail.com>
2023-04-05 11:34:51 -0400

    :lipstick: Adding hamburger menu on movile screen size

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,43 +1,57 @@
+import { useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import Route from '../../routes/routes';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <ul className="navbar--list">
-      <li className="navbar--list__item">
-        <NavLink
-          className={({ isActive }) => (isActive ? 'active' : 'link')}
-          to={Route.Home}
-        >
-          Home
-        </NavLink>
-      </li>
-      <li className="navbar--list__item">
-        <NavLink
-          className={({ isActive }) => (isActive ? 'active' : 'link')}
-          to={Route.Characters}
-        >
-          Characters
-        </NavLink>
-      </li>
-      <li className="navbar--list__item">
-        <NavLink
-          className={({ isActive }) => (isActive ? 'active' : 'link')}
-          to={Route.Comics}
-        >
-          Comics
-        </NavLink>
-      </li>
-      <li className="navbar--list__item">
-        <NavLink
-          className={({ isActive }) => (isActive ? 'active' : 'link')}
-          to={Route.Stories}
-        >
-          Stories
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const buttonRef = useRef<HTMLDivElement | null>(null);
+  const unorderedListRef = useRef<HTMLUListElement | null>(null);
+
+  const handleClick = () => {
+    buttonRef.current?.classList.toggle('open');
+    unorderedListRef.current?.classList.toggle('open');
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar--btn" onClick={handleClick}>
+        <div className="navbar--btn__menu" ref={buttonRef} />
+      </div>
+      <ul className="navbar--list" ref={unorderedListRef}>
+        <li className="navbar--list__item">
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : 'link')}
+            to={Route.Home}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="navbar--list__item">
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : 'link')}
+            to={Route.Characters}
+          >
+            Characters
+          </NavLink>
+        </li>
+        <li className="navbar--list__item">
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : 'link')}
+            to={Route.Comics}
+          >
+            Comics
+          </NavLink>
+        </li>
+        <li className="navbar--list__item">
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : 'link')}
+            to={Route.Stories}
+          >
+            Stories
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
